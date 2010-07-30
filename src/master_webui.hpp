85cd5d2dc718ff1f61b338facfa7db6795ec8d12
Andy Konwinski <andyk@berkeley.edu>
2010-07-30 12:04:26 -0700

    Incremental commit of weui logs bugfix, causes segfault (may not compile).

--- src/master_webui.hpp
+++ src/master_webui.hpp
@@ -5,12 +5,13 @@
 
 #include "config.hpp"
 #include "master.hpp"
+#include "configurator.hpp"
 
 #ifdef MESOS_WEBUI
 
 namespace mesos { namespace internal { namespace master {
 
-void startMasterWebUI(const PID &master, char* webuiPort);
+void startMasterWebUI(const PID &master, Params &params);
 
 }}} /* namespace */
 
